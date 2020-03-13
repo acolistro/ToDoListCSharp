64d4f89d97af85715913af5209a7da42e780410a
acolistro <arcolistro@gmail.com>
2020-03-12 20:20:32 -0700

    add to home and items models and controllers.

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -7,6 +7,7 @@ namespace ToDoList.Controllers
 {
   public class CategoriesController : Controller
   {
+
     [HttpGet("/categories")]
     public ActionResult Index()
     {
@@ -14,17 +15,17 @@ namespace ToDoList.Controllers
       return View(allCategories);
     }
 
+    [HttpGet("/categories/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
     [HttpPost("/categories")]
     public ActionResult Create(string categoryName)
     {
       Category newCategory = new Category(categoryName);
       return RedirectToAction("Index");
-    }
-
-    [HttpGet("/categories/new")]
-    public ActionResult New()
-    {
-      return View();
     }
 
     [HttpGet("/categories/{id}")]
@@ -36,6 +37,20 @@ namespace ToDoList.Controllers
       model.Add("category", selectedCategory);
       model.Add("items", categoryItems);
       return View(model);
+    }
+
+    // This one creates new Items within a given Category, not new Categories:
+    [HttpPost("/categories/{categoryId}/items")]
+    public ActionResult Create(int categoryId, string itemDescription)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category foundCategory = Category.Find(categoryId);
+      Item newItem = new Item(itemDescription);
+      foundCategory.AddItem(newItem);
+      List<Item> categoryItems = foundCategory.Items;
+      model.Add("items", categoryItems);
+      model.Add("category", foundCategory);
+      return View("Show", model);
     }
 
   }
